0ee928181fc6e294890593678f3a389d899b90ef
gabriels17 <gabriel.sighvatsson@gmail.com>
2020-02-21 16:21:23 +0000

    test script

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -5,4 +5,4 @@ set -e
 GIT_COMMIT=$1
 
 docker build -t gabriels17/stafsetning-api:$GIT_COMMIT .
-docker push gabriels17/stafsetning-api:$GIT_COMMIT
+# docker push gabriels17/stafsetning-api:$GIT_COMMIT
