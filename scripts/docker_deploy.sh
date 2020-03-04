9d9cb1b587bf59bb881a379b94e685fb78a07279
gabriels17 <gabriel.sighvatsson@gmail.com>
2020-03-04 22:38:48 +0000

    script change

--- scripts/docker_deploy.sh
+++ scripts/docker_deploy.sh
@@ -8,7 +8,5 @@ REGISTRY_PASS=$3
 GIT_COMMIT=$4
 
 docker login -u $REGISTRY_USER -p $REGISTRY_PASS
-docker tag $IMAGE_NAME $IMAGE_NAME:latest
-docker tag $IMAGE_NAME $IMAGE_NAME:$GIT_COMMIT
-docker build $IMAGE_NAME:$GIT_COMMIT .
+docker build -t $IMAGE_NAME:$GIT_COMMIT -t $IMAGE_NAME $IMAGE_NAME:latest .
 docker push $IMAGE_NAME:$GIT_COMMIT && docker push $IMAGE_NAME:latest
