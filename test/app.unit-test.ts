9dc3abdff44c7072a024d862d2b322aa8f86acaa
gabriels17 <gabriel.sighvatsson@gmail.com>
2020-03-11 16:29:26 +0000

    Changed test config

--- test/app.unit-test.ts
+++ test/app.unit-test.ts
@@ -1,15 +1,15 @@
 import request from "supertest";
 import app from "../src/app";
 
-describe("GET /random-url", () => {
+describe("GET /status", () => {
     it("should return 404", (done) => {
-        request(app).get("/reset")
-            .expect(404, done);
+        request(app).get("/status")
+            .expect(200, done);
     });
 });
 
 describe("testing CI", () => {
     it("should work", () => {
-        expect(5).toEqual(3);
+        expect(5).toEqual(5);
     });
 });
