bdc5e8491831926f6347476b71a9969ebd1f570c
njall skarphedinsson <njallis@gmail.com>
2020-03-13 10:19:27 +0000

    created exercise

--- src/models/Exercises/index.ts
+++ src/models/Exercises/index.ts
@@ -1,31 +1,41 @@
-import { model, Schema, Mongoose } from "mongoose";
-import {ExerciseCollectionInterface, ExerciseInterface } from "./interface";
+import { model, Schema, Mongoose, HookNextFunction, Collection } from "mongoose";
+import { ExerciseCollectionInterface, ExerciseInterface } from "./interface";
 import * as statics from "./statics";
 import * as methods from "./methods";
 
-const exerciseSchema = new Schema({
-	age: {
-		type: Number,
-		required: true
+const exerciseSchema = new Schema(
+	{
+		difficultRange: {
+			type: [Number],
+			required: [true, "Æfing verður að hafa erfiðleikastig"],
+			validate: {
+				validator: function(value: number[]) {
+					if (value.length !== 2) return false;
+					const [min, max] = value;
+					return min <= max;
+				},
+				msg: "Hámarks og lágmarks erfiðleikastig ekki rétt"
+			}
+		},
+		number: {
+			type: Number,
+		}
 	},
-	name: {
-		type: String,
-		required: true
-	},
-	mother: {
-		type: Schema.Types.ObjectId,
-		ref: "person",
-		required: false
-	}
-}, { timestamps: true });
+	{ timestamps: true }
+);
 
-exerciseSchema.statics = statics;
+exerciseSchema.statics	 = statics;
 exerciseSchema.methods = methods;
+exerciseSchema.post("save", async function() {
+	this.number = await Collection.countDocuments();
+});
 
-export const Person = model<ExerciseInterface, ExerciseCollectionInterface>(
+export const Exercises = model<ExerciseInterface, ExerciseCollectionInterface>(
 	"exercises",
 	exerciseSchema,
 	"exercises"
 );
+
+
 
 export * from "./interface";
