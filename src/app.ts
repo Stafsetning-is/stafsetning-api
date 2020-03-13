7d935a6d8286e796d956fdb94197c12f55071a3d
gabriels17 <gabriel.sighvatsson@gmail.com>
2020-03-13 13:59:36 +0000

    Lint issues

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import mongo from "connect-mongo";
 import path from "path";
 import mongoose from "mongoose";
 import { MONGODB_URI, SESSION_SECRET } from "./util/secrets";
-import Router from "./routes"
+import Router from "./routes";
 import cors from "cors";
 import "firebase/auth";
 
@@ -22,7 +22,7 @@ import "firebase/auth";
 
 // Create Express server
 const app = express();
-app.use(cors())
+app.use(cors());
 
 // Connect to MongoDB
 const mongoUrl = MONGODB_URI;
@@ -31,7 +31,7 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useCreateIndex: true, useUni
     () => {
         console.log("Connected to MongoDB");
     }
-).catch((err: any) => {
+).catch((err: Error) => {
     console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
 });
 
@@ -66,21 +66,21 @@ app.get("/status", (req, res) => {
     res.send("API is up and running...");
 });
 
-const authorizeUser = (uid: string) => {
-    admin.auth().createCustomToken(uid)
-        .then((customToken) => {
-            return customToken;
-        })
-        .catch((error) => {
-            console.log("Error creating custom token:", error);
-        });
-};
+// const authorizeUser = (uid: string) => {
+//     admin.auth().createCustomToken(uid)
+//         .then((customToken) => {
+//             return customToken;
+//         })
+//         .catch((error) => {
+//             console.log("Error creating custom token:", error);
+//         });
+// };
 
 app.use("/", Router);
 
 app.get("*", (req, res) => {
-    res.status(404).send()
-})
+    res.status(404).send();
+});
 
 // Default: Not supported
 app.use("*", (req, res) => {
