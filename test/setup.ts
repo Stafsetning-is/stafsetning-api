b231969a5ed5c37f18f25a1b0220a0056239f383
gabriels17 <gabriel.sighvatsson@gmail.com>
2020-03-14 02:45:01 +0000

    Refactored mongo test setup

--- test/setup.ts
+++ test/setup.ts
@@ -1,6 +1,13 @@
 import mongoose from "mongoose";
 import { MongoMemoryServer } from "mongodb-memory-server";
 
+mongoose.Promise = Promise;
+
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
+
 /**
  * Setup to connect mongoose to mongo-memory-server
  * for tests
@@ -8,26 +15,8 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 beforeAll((done) => {
 	const mongoServer = new MongoMemoryServer();
 
-	mongoose.Promise = Promise;
 	mongoServer.getUri().then((mongoUri) => {
-		const mongooseOpts = {
-			// options for mongoose 4.11.3 and above
-			autoReconnect: true,
-			reconnectTries: Number.MAX_VALUE,
-			reconnectInterval: 1000,
-			useMongoClient: true // remove this line if you use mongoose 5 and above
-		};
-
-		mongoose.connect(mongoUri, mongooseOpts);
-
-		mongoose.connection.on("error", (e) => {
-			if (e.message.code === "ETIMEDOUT") {
-				console.log(e);
-				mongoose.connect(mongoUri, mongooseOpts);
-			}
-			console.log(e);
-		});
-
+		mongoose.connect(mongoUri);
 		mongoose.connection.once("open", () => {
             console.log(`MongoDB successfully connected to ${mongoUri}`);
             done();
