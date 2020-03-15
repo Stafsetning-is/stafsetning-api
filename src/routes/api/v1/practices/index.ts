0d8d0a431f5ace8f45b2b3b9681505288c103157
njall skarphedinsson <njallis@gmail.com>
2020-03-15 14:38:42 +0000

    added get practice by id route

--- src/routes/api/v1/practices/index.ts
+++ src/routes/api/v1/practices/index.ts
@@ -1,4 +1,5 @@
 import post from "./post";
+import getById from "./get_by_id";
 import { RouteBuilder } from "../../../utils";
 
 export default RouteBuilder.routerForEndpoints([
@@ -6,6 +7,11 @@ export default RouteBuilder.routerForEndpoints([
         route: "/",
         controller: post,
         method: "post"
+    },
+    {
+        route: "/:id",
+        controller: getById,
+        method: "get"
     },
 ]);
 
