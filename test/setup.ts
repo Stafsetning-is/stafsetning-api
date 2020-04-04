e4c136fe2a0a55581d82af9c001832f0526cba49
njall skarphedinsson <njallis@gmail.com>
2020-04-04 18:09:00 +0000

    added sign in fail case test

--- test/setup.ts
+++ test/setup.ts
@@ -1,20 +1,33 @@
 import mongoose from "mongoose";
 import { MongoMemoryServer } from "mongodb-memory-server";
+import { Users } from "../src/models";
 
 let mongoServer: MongoMemoryServer;
 
 beforeAll(async () => {
-  mongoServer = new MongoMemoryServer();
-  const mongoUri = await mongoServer.getUri();
-  await mongoose.connect(mongoUri, {
-	  useNewUrlParser: true,
-	  useUnifiedTopology: true
-  }, (err) => {
-    if (err) console.error(err);
-  });
+	mongoServer = new MongoMemoryServer();
+	const mongoUri = await mongoServer.getUri();
+	await mongoose.connect(
+		mongoUri,
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		},
+		(err) => {
+			if (err) console.error(err);
+		}
+	);
+	await Users.create({
+		name: "Pope John Paul II",
+		password: "Password12.3",
+		mobile: "5812345",
+		type: "user",
+		username: "paulyp",
+		difficulty: 5,
+	});
 });
 
 afterAll(async () => {
-  await mongoose.disconnect();
-  await mongoServer.stop();
+	await mongoose.disconnect();
+	await mongoServer.stop();
 });
