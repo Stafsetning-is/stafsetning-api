5594db4ead3988b22edeb979369a7e5c0847fc41
njall skarphedinsson <njallis@gmail.com>
2020-04-04 13:35:16 +0000

    added rate limitt middle ware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import compression from "compression";  // compresses requests
+import compression from "compression"; // compresses requests
 import session from "express-session";
 import bodyParser from "body-parser";
 import mongo from "connect-mongo";
@@ -8,7 +8,7 @@ import mongoose from "mongoose";
 import { MONGODB_URI, SESSION_SECRET } from "./util/secrets";
 import Router from "./routes";
 import cors from "cors";
-
+import rateLimit from "express-rate-limit";
 // Create Express server
 const app = express();
 app.use(cors());
@@ -16,25 +16,41 @@ app.use(cors());
 // Connect to MongoDB
 const mongoUrl = MONGODB_URI;
 const MongoStore = mongo(session);
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true } ).then(
-    () => {
-        console.log("Connected to MongoDB");
-    }
-).catch((err: Error) => {
-    console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
-});
+mongoose
+	.connect(mongoUrl, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log("Connected to MongoDB");
+	})
+	.catch((err: Error) => {
+		console.log(
+			"MongoDB connection error. Please make sure MongoDB is running. " + err
+		);
+	});
 
 // Mongo middleware to express
-app.use(session({
-    resave: true,
-    saveUninitialized: true,
-    secret: SESSION_SECRET,
-    store: new MongoStore({
-        url: mongoUrl,
-        autoReconnect: true
-    })
-}));
+app.use(
+	session({
+		resave: true,
+		saveUninitialized: true,
+		secret: SESSION_SECRET,
+		store: new MongoStore({
+			url: mongoUrl,
+			autoReconnect: true,
+		}),
+	})
+);
 
+// request rate limits from same ip's
+app.use(
+	rateLimit({
+		windowMs: 15 * 60 * 1000, // 15 minutes
+		max: 100, // limit each IP to 100 requests per windowMs
+	})
+);
 
 // Express configuration
 app.set("port", process.env.PORT || 5000);
@@ -42,28 +58,27 @@ app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 app.use(
-    express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
+	express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
 );
 
 app.get("/", (_req, res) => {
-    res.send("Hello from the API!");
+	res.send("Hello from the API!");
 });
 
 app.get("/status", (_req, res) => {
-    res.send("API is up and running...");
+	res.send("API is up and running...");
 });
 
 app.get("/coffee", (_req, res) => {
-    res.status(418).send("You attempted to brew coffee with a teapot.");
+	res.status(418).send("You attempted to brew coffee with a teapot.");
 });
 
 // connect routes to app
 app.use("/", Router);
 
 app.get("*", (_req, res) => {
-    res.status(404).send();
+	res.status(404).send();
 });
 
 export default app;
