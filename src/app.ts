3d40645759571b201fcc95fa62827e33334ac1ab
Kjartan <kjartano17@ru.is>
2020-04-05 12:09:50 +0000

    added database connection

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import compression from "compression";  // compresses requests
+import compression from "compression"; // compresses requests
 import session from "express-session";
 import bodyParser from "body-parser";
 import mongo from "connect-mongo";
@@ -16,32 +16,40 @@ app.use(cors());
 // Connect to MongoDB
 const mongoUrl = MONGODB_URI;
 const MongoStore = mongo(session);
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true } ).then(
-    () => {
-        console.log("Connected to MongoDB");
-    }
-).catch((err: Error) => {
-    console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
-});
+mongoose
+    .connect(mongoUrl, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Connected to MongoDB", mongoUrl);
+    })
+    .catch((err: Error) => {
+        console.log(
+            "MongoDB connection error. Please make sure MongoDB is running. " +
+                err
+        );
+    });
 
 // Mongo middleware to express
-app.use(session({
-    resave: true,
-    saveUninitialized: true,
-    secret: SESSION_SECRET,
-    store: new MongoStore({
-        url: mongoUrl,
-        autoReconnect: true
+app.use(
+    session({
+        resave: true,
+        saveUninitialized: true,
+        secret: SESSION_SECRET,
+        store: new MongoStore({
+            url: mongoUrl,
+            autoReconnect: true
+        })
     })
-}));
-
+);
 
 // Express configuration
 app.set("port", process.env.PORT || 5000);
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 
 app.use(
     express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
