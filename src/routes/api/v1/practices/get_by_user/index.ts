46b6ffa8e6bee2379c866e25aa54a84834f0adf6
kjartan <kjartan.reynir.olafsson@gmail.com>
2020-04-14 23:10:21 +0000

    get practice by user works

--- src/routes/api/v1/practices/get_by_user/index.ts
+++ src/routes/api/v1/practices/get_by_user/index.ts
@@ -1,9 +1,18 @@
-import { Practices, Exercises } from "../../../../../models";
+import { Practices } from "../../../../../models";
 import { Request, Response } from "express";
 
 export default async (req: Request, res: Response) => {
     try {
-        res.send([]);
+        const docs = await Practices.find({ user: req.body.user._id }).populate(
+            "exercise"
+        );
+        const exercises = docs.map((doc) => {
+            const exercise = doc.exercise;
+            exercise.completed = true;
+            exercise.practice = doc._id;
+            return exercise;
+        });
+        res.send(exercises);
     } catch (error) {
         res.status(400).send(error);
     }
