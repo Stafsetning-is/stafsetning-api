d7a2e75d7262c840dc4a49b517333842a453377b
njall skarphedinsson <njallis@gmail.com>
2020-04-18 18:40:23 +0000

    added routerForEndpoints for /admin/users

---
+++ src/routes/api/admin/users/index.ts
@@ -0,0 +1,10 @@
+import makeAdmin from "./make_admin";
+import { RouteBuilder } from "../../utils";
+
+export default RouteBuilder.routerForEndpoints([
+	{
+		route: "/:uid/make_admin/",
+		controller: makeAdmin,
+		method: "post",
+	},
+]);
