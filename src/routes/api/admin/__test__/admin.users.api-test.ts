c1632aa882e6f83b3f2846716436c0f85316188c
njall skarphedinsson <njallis@gmail.com>
2020-04-18 19:01:33 +0000

    admin routes connection finally works

--- src/routes/api/admin/__test__/admin.users.api-test.ts
+++ src/routes/api/admin/__test__/admin.users.api-test.ts
@@ -2,33 +2,28 @@ import request from "supertest";
 import app from "../../../../app";
 
 describe("User routes", () => {
-	it(`GET /api/admin/users/${app.get(
-		"userId"
-	)}/make_admin requires auth`, async (done) => {
-		const { status } = await request(app).get(
-			"/api/admin/users/:uid/make_admin"
+	it("POST /api/admin/users/:uid/make_admin requires auth", async (done) => {
+		const { status } = await request(app).post(
+			`/api/admin/users/${app.get("userId")}/make_admin`
 		);
 		expect(status).toEqual(401);
 		done();
 	});
 
-	it(`GET /api/admin/users/${app.get(
-		"userId"
-	)}/make_admin requires does not accept user tokens`, async (done) => {
+	it("POST /api/admin/users/:uid/make_admin does work with admin token", async (done) => {
 		const token = app.get("testToken");
 		const { status } = await request(app)
-			.get("/api/admin/users/:uid/make_admin")
+			.post(`/api/admin/users/${app.get("userId")}/make_admin`)
 			.set({ Authorization: `Bearer ${token}` });
 		expect(status).toEqual(401);
 		done();
 	});
 
-	it(`GET /api/admin/users/${app.get(
-		"userId"
-	)}/make_admin does work with admin token`, async (done) => {
+	it("POST /api/admin/users/:uid/make_admin does work with admin token", async (done) => {
 		const token = app.get("adminTestToken");
+		console.log(`/api/admin/users/${app.get("userId")}/make_admin`);
 		const { status } = await request(app)
-			.get("/api/admin/users/:uid/make_admin")
+			.post(`/api/admin/users/${app.get("userId")}/make_admin`)
 			.set({ Authorization: `Bearer ${token}` });
 		expect(status).toEqual(200);
 		done();
--- src/routes/api/admin/users/index.ts
+++ src/routes/api/admin/users/index.ts
@@ -3,7 +3,7 @@ import { RouteBuilder } from "../../../utils";
 
 export default RouteBuilder.routerForEndpoints([
 	{
-		route: "/:uid/make_admin/",
+		route: "/:uid/make_admin",
 		controller: makeAdmin,
 		method: "post",
 	},
