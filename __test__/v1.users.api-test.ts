250169b3f0df1b22b8048f82366fd2bcd8dfcf9b
njall skarphedinsson <njallis@gmail.com>
2020-04-18 17:28:58 +0000

    fixed test setup

--- __test__/v1.users.api-test.ts
+++
@@ -1,5 +0,0 @@
-// copy paste from other test suite to get setup
-// create endpoind tests for /api/v1/users/change_difficulty
-// bua atil endpooint
-// bua til test fyrir invalid difficulty t.d. "a", 17
-// vid erum ad nota mongoose til ad tala vid DB
