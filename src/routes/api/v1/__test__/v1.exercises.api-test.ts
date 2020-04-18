9385cadd36eb63ad16ba25e3bbc830652fae3677
njall skarphedinsson <njallis@gmail.com>
2020-04-18 18:26:13 +0000

    relocated tests

--- src/routes/api/v1/__test__/v1.exercises.api-test.ts
+++ src/routes/api/v1/__test__/v1.exercises.api-test.ts
@@ -1,5 +1,5 @@
 import request from "supertest";
-import app from "../src/app";
+import app from "../../../../app";
 
 describe("Exercise Routes V1", () => {
 	it("GET /api/v1/exercises/by_difficulty/ requires auth", async (done) => {
