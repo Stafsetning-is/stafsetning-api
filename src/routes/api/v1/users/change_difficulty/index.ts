f2a4c4884baaf7ab62eea08a90fb47ab283a0006
kjartan <kjartan.reynir.olafsson@gmail.com>
2020-04-21 12:01:30 +0000

    route functional

--- src/routes/api/v1/users/change_difficulty/index.ts
+++ src/routes/api/v1/users/change_difficulty/index.ts
@@ -1,8 +1,5 @@
 import { Request, Response } from "express";
 
-/**
- * Returns the user that sent the token
- */
 export default async (req: Request, res: Response) => {
     try {
         res.send(req.body.user);
