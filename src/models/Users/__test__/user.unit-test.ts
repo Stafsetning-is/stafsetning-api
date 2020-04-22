b3ab8dee4ef57e70ce9e5902b4f94e10c32d91fe
Kjartan Reynir Ólafsson <kjartano17@ru.is>
2020-04-22 10:55:45 +0000

    testing update difficulty functional

--- src/models/Users/__test__/user.unit-test.ts
+++ src/models/Users/__test__/user.unit-test.ts
@@ -4,89 +4,138 @@ import { Users, UserInterface } from "../index";
 let user: UserInterface;
 
 beforeAll(async (done) => {
-	user = await Users.create({
-		name: "Tester 5",
-		password: "some password",
-		username: "testington7",
-		mobile: "5830234",
-		difficulty: 4,
-	});
-	done();
+    user = await Users.create({
+        name: "Tester 5",
+        password: "some password",
+        username: "testington7",
+        mobile: "5830234",
+        difficulty: 4,
+    });
+    done();
 });
 
 describe("User creation", () => {
-	it("Should hash password when created", async (done) => {
-		const instance = await Users.create({
-			name: "Tester 5",
-			password: "some password",
-			username: "testington",
-			mobile: "5810234",
-			difficulty: 4,
-		});
-		expect(instance.password).not.toEqual("some password");
-		done();
-	});
+    it("Should hash password when created", async (done) => {
+        const instance = await Users.create({
+            name: "Tester 5",
+            password: "some password",
+            username: "testington",
+            mobile: "5810234",
+            difficulty: 4,
+        });
+        expect(instance.password).not.toEqual("some password");
+        done();
+    });
 
-	it("Should not allow difficulty out of range", async (done) => {
-		const create1 = async () => {
-			try {
-				await Users.create({
-					name: "Tester 5",
-					password: "some password",
-					username: "testington1",
-					mobile: "5810235",
-					difficulty: 0,
-				});
-			} catch (error) {
-				throw Error("test");
-			}
-		};
-		const create2 = async () => {
-			try {
-				await Users.create({
-					name: "Tester 5",
-					password: "some password",
-					username: "testington2",
-					mobile: "5810236",
-					difficulty: 12,
-				});
-			} catch (error) {
-				throw Error("test");
-			}
-		};
-		expect(create1()).rejects.toEqual(new Error("test"));
-		expect(create2()).rejects.toEqual(new Error("test"));
-		done();
-	});
+    it("Should not allow difficulty out of range", async (done) => {
+        const create1 = async () => {
+            try {
+                await Users.create({
+                    name: "Tester 5",
+                    password: "some password",
+                    username: "testington1",
+                    mobile: "5810235",
+                    difficulty: 0,
+                });
+            } catch (error) {
+                throw Error("test");
+            }
+        };
+        const create2 = async () => {
+            try {
+                await Users.create({
+                    name: "Tester 5",
+                    password: "some password",
+                    username: "testington2",
+                    mobile: "5810236",
+                    difficulty: 12,
+                });
+            } catch (error) {
+                throw Error("test");
+            }
+        };
+        await expect(create1()).rejects.toEqual(new Error("test"));
+        await expect(create2()).rejects.toEqual(new Error("test"));
+        done();
+    });
 
-	it("Should not allow creation of admins", async (done) => {
-		const user = await Users.create({
-			name: "Tester 5",
-			password: "some password",
-			username: "testington2",
-			mobile: "5810236",
-			difficulty: 10,
-			type: "admin",
-		});
-		expect(user.type).toBe("user");
-		done();
-	});
+    it("Should not allow creation of admins", async (done) => {
+        const user = await Users.create({
+            name: "Tester 5",
+            password: "some password",
+            username: "testington2",
+            mobile: "5810236",
+            difficulty: 10,
+            type: "admin",
+        });
+        expect(user.type).toBe("user");
+        done();
+    });
 });
 
 describe("User methods", () => {
-	it("should upgrade priveledges on .makeAdmin()", async (done) => {
-		await user.makeAdmin();
-		const found = await Users.findById(user._id);
-		expect(found.type).toBe("admin");
-		done();
-	});
+    it("should upgrade priveledges on .makeAdmin()", async (done) => {
+        await user.makeAdmin();
+        const found = await Users.findById(user._id);
+        expect(found.type).toBe("admin");
+        done();
+    });
 
-	it("should generate token", async () => {
-		const token = await user.generateAuthToken();
-		const found = await Users.findOne({
-			_id: user._id,
-			"tokens.token": token,
-		});
-		expect(found._id.toString()).toBe(user._id.toString());
-	});
+    it("should generate token", async (done) => {
+        const token = await user.generateAuthToken();
+        const found = await Users.findOne({
+            _id: user._id,
+            "tokens.token": token,
+        });
+        expect(found._id.toString()).toBe(user._id.toString());
+        done();
+    });
+
+    // it("should not allow changing difficulty to out of range", async () => {
+    //     const updateLower = async () => {
+    //         try {
+    //             await Users.findOneAndUpdate(
+    //                 { _id: user._id },
+    //                 { difficulty: 0 }
+    //             );
+    //         } catch (error) {
+    //             throw Error("test");
+    //         }
+    //     };
+    //     const updateHiger = async () => {
+    //         try {
+    //             await Users.findOneAndUpdate(
+    //                 { _id: user._id },
+    //                 { difficulty: 11 }
+    //             );
+    //         } catch (error) {
+    //             // console.log(user.difficulty);
+    //             throw Error("test");
+    //         }
+    //     };
+    //     expect(updateLower()).rejects.toEqual(new Error("test"));
+    //     expect(updateHiger()).rejects.toEqual(new Error("test"));
+    // });
+
+    it("should not update difficulty value", async (done) => {
+        const update1 = async () => {
+            try {
+                user.difficulty = 12;
+                await user.save();
+            } catch (error) {
+                throw Error("test");
+            }
+        };
+        const update2 = async () => {
+            try {
+                user.difficulty = 0;
+                await user.save();
+            } catch (error) {
+                throw Error("test");
+            }
+        };
+        await expect(update1()).rejects.toEqual(new Error("test"));
+        await expect(update2()).rejects.toEqual(new Error("test"));
+        done();
+    });
 });
--- src/routes/api/v1/__test__/v1.users.api-test.ts
+++ src/routes/api/v1/__test__/v1.users.api-test.ts
@@ -28,13 +28,27 @@ describe("Auth routes", () => {
         done();
     });
 
-    // it("GET /api/v1/users/change_difficulty responds with 200 with correct token", async (done) => {
+    // it("POST /api/v1/users/change_difficulty responds with 200 with correct token", async (done) => {
     //     const token = app.get("testToken");
-    //     const { body } = await request(app)
-    //         .get("/api/v1/users/change_difficulty")
+    //     let userDiff = { difficulty: 5 };
+    //     const { body, status } = await request(app)
+    //         .post("/api/v1/users/change_difficulty")
+    //         .send(userDiff)
     //         .set({ Authorization: `Bearer ${token}` });
-    //     expect(body).toHaveProperty("_id");
     //     expect(body).toHaveProperty("difficulty");
+    //     expect(status).toEqual(200);
+    //     done();
+    // });
+
+    // it("POST /api/v1/users/change_difficulty should not allow difficulty zero", async (done) => {
+    //     const token = app.get("testToken");
+    //     let userDiff = { difficulty: 0 };
+    //     const { body, status } = await request(app)
+    //         .post("/api/v1/users/change_difficulty")
+    //         .send(userDiff)
+    //         .set({ Authorization: `Bearer ${token}` });
+    //     // expect(body).toHaveProperty("difficulty");
+    //     expect(body).toEqual(400);
     //     done();
     // });
 });
