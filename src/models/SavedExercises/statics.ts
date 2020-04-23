60519c07e49a7b128f9286aa3e2f943fd409fb46
njall skarphedinsson <njallis@gmail.com>
2020-04-23 13:36:56 +0000

    added deleteSave

--- src/models/SavedExercises/statics.ts
+++ src/models/SavedExercises/statics.ts
@@ -31,3 +31,25 @@ export const createNew = async function (
 		throw new Error("Can't save this exercise twice for same user");
 	}
 };
+
+/**
+ * deletes and saved exercise
+ * @param this is just ta type decleration for this, has no outside effect
+ * @param user the user wishing to unsave
+ * @param exercise exercise to unsave for user
+ */
+export const deleteSave = async function (
+	this: SavedExercisesCollectionInterface,
+	user: StringOrObjectId,
+	exercise: StringOrObjectId
+) {
+	// verifies inputs
+	[user, exercise].forEach((input) => verifyObjectId(input));
+
+	try {
+		const doc = await SavedExercises.findOneAndDelete({ user, exercise });
+		if (!doc) throw Error();
+	} catch (error) {
+		throw new Error("Can't find saved exercise");
+	}
+};
