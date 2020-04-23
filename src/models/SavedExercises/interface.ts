42b3266698ec58b14646489022af0586c0acc163
njall skarphedinsson <njallis@gmail.com>
2020-04-23 13:11:36 +0000

    added interfce for collection

--- src/models/SavedExercises/interface.ts
+++ src/models/SavedExercises/interface.ts
@@ -1,6 +1,16 @@
-import { Types, Document } from "mongoose";
+import { Types, Document, Model } from "mongoose";
+
+type StringOrObjectId = string | Types.ObjectId;
 
 export interface SavedExercisesInterface extends Document {
 	user: Types.ObjectId;
 	exercise: Types.ObjectId;
+}
+
+export interface SavedExercisesCollectionInterface
+	extends Model<SavedExercisesInterface> {
+	createNew: (
+		user: StringOrObjectId,
+		exercise: StringOrObjectId
+	) => Promise<SavedExercisesInterface>;
 }
