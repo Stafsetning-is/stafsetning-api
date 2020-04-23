f1319de41363057da7dcb44f9a54877b6e6b3704
njall skarphedinsson <njallis@gmail.com>
2020-04-23 15:37:46 +0000

    added saved property to response

--- src/routes/api/v1/exercises/by_difficulty/utils.ts
+++ src/routes/api/v1/exercises/by_difficulty/utils.ts
@@ -1,7 +1,13 @@
-import { PublicUser, ExerciseRepr } from "../../../../../models";
+import {
+	PublicUser,
+	ExerciseRepr,
+	SavedExercises,
+	FinishedExerciseRepr,
+} from "../../../../../models";
 import { Exercises } from "../../../../../models";
 import { ExerciseReprDict } from "./interface";
 
+type AwaitAllRes = [ExerciseRepr[], ExerciseRepr[], ExerciseRepr[]];
 /**
  * Fetches exercises in users diffculty range
  * then it fetches completed exercises by user's id
@@ -12,14 +18,31 @@ export const getExercisesForUser = async ({
 	difficulty,
 	_id,
 }: PublicUser): Promise<ExerciseRepr[]> => {
-	const [all, finished] = await Promise.all([
+	/**
+	 * Await necessary data lookups
+	 * in parallel by promise all
+	 */
+	const [all, finished, saved]: AwaitAllRes = await Promise.all([
 		Exercises.getExercisesByDifficulty(difficulty),
 		Exercises.getCompletedExercises(_id, true),
+		SavedExercises.getExercisesSavedByUser(_id),
 	]);
+
+	// maps to dict to make sure no duplicate _ids
 	const dict = [...all, ...finished].reduce<ExerciseReprDict>((prev, curr) => {
-		prev[curr._id.toString()] = curr;
+		const key = curr._id.toString();
+		prev[key] = curr;
+		prev[key].saved = false;
 		return prev;
 	}, {});
+
+	// marks saved if they're fund in savedexercise collection
+	saved.forEach(({ _id }) => {
+		const key = _id.toString();
+		if (dict[key]) dict[key].saved = true;
+	});
+
+	// returns objects
 	return Object.keys(dict)
 		.map((key) => dict[key])
 		.sort((a, b) => {
