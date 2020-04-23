141cf8e2616f7a5deac018ade863cd7b4fbcaad7
njall skarphedinsson <njallis@gmail.com>
2020-04-23 13:23:28 +0000

    added method to route

--- src/routes/api/v1/users/save_exercise/index.ts
+++ src/routes/api/v1/users/save_exercise/index.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from "express";
+import { SavedExercises } from "../../../../../models";
 /**
  * This method finds user by id and updates difficulty
  */
 export default async (req: Request, res: Response) => {
 	try {
 		const { id } = req.params;
+		const doc = await SavedExercises.createNew(req.body.user._id, id);
+		res.send(doc);
 	} catch (error) {
 		res.status(400).send({ message: error.message });
 	}
