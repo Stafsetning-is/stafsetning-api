919c8c3af27300a157fa370461c895c79474cb69
njall skarphedinsson <njallis@gmail.com>
2020-04-25 17:51:31 +0000

    added update route to app

--- src/routes/api/admin/exercises/index.ts
+++ src/routes/api/admin/exercises/index.ts
@@ -1,10 +1,17 @@
 import create from "./create";
+import update from "./update";
+
 import { RouteBuilder } from "../../../utils";
 
 export default RouteBuilder.routerForEndpoints([
 	{
 		route: "/",
 		controller: create,
+		method: "post",
+	},
+	{
+		route: "/update",
+		controller: update,
 		method: "post",
 	},
 ]);
