6dc3cacd2f5e16f884f7f4cdeecece9d0a040215
njall skarphedinsson <njallis@gmail.com>
2020-04-25 17:05:02 +0000

    added const instead of number in response

--- src/routes/api/open/exercise_sample/index.ts
+++ src/routes/api/open/exercise_sample/index.ts
@@ -1,10 +1,12 @@
 import { Exercises } from "../../../../models";
 import { Request, Response } from "express";
 
+const DOCUMENT_COUNT;
+
 export default async (req: Request, res: Response) => {
 	const docs = await Exercises.find({
 		published: true,
 		deleted: false,
-	}).limit(20);
+	}).limit(DOCUMENT_COUNT);
 	res.send(docs.map((doc) => doc.getRepresentation()));
 };
