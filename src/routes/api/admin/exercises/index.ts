df46779991f02c3b53e7b3b715fa11b02f7d1694
njall skarphedinsson <njallis@gmail.com>
2020-04-25 17:10:22 +0000

    connected route

---
+++ src/routes/api/admin/exercises/index.ts
@@ -0,0 +1,10 @@
+import create from "./create";
+import { RouteBuilder } from "../../../utils";
+
+export default RouteBuilder.routerForEndpoints([
+	{
+		route: "/",
+		controller: create,
+		method: "post",
+	},
+]);
