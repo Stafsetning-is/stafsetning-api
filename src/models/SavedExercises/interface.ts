37174db7f3dd9616fadcd227c62d1c2155cf1634
nannalop <natalia17@ru.is>
2020-04-26 20:42:43 +0000

    drasl

--- src/models/SavedExercises/interface.ts
+++ src/models/SavedExercises/interface.ts
@@ -4,19 +4,21 @@ import { ExerciseInterface, ExerciseRepr } from "../Exercises";
 export type StringOrObjectId = string | Types.ObjectId;
 
 export interface SavedExercisesInterface extends Document {
-	user: Types.ObjectId;
-	exercise: ExerciseInterface;
+    user: Types.ObjectId;
+    exercise: ExerciseInterface;
 }
 
 export interface SavedExercisesCollectionInterface
-	extends Model<SavedExercisesInterface> {
-	createNew: (
-		user: StringOrObjectId,
-		exercise: StringOrObjectId
-	) => Promise<SavedExercisesInterface>;
-	deleteSave: (
-		user: StringOrObjectId,
-		exercise: StringOrObjectId
-	) => Promise<void>;
-	getExercisesSavedByUser: (user: StringOrObjectId) => Promise<ExerciseRepr[]>;
+    extends Model<SavedExercisesInterface> {
+    createNew: (
+        user: StringOrObjectId,
+        exercise: StringOrObjectId
+    ) => Promise<SavedExercisesInterface>;
+    deleteSave: (
+        user: StringOrObjectId,
+        exercise: StringOrObjectId
+    ) => Promise<void>;
+    getExercisesSavedByUser: (
+        user: StringOrObjectId
+    ) => Promise<ExerciseRepr[]>;
 }
