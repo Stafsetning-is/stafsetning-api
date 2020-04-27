96c37f8cdf8a49f4f615db02d1bd7af54b6051a1
nannalop <natalia17@ru.is>
2020-04-27 19:01:35 +0000

    fix- Typescript too strict in my config

--- src/routes/api/admin/exercises/get_by_file_name/index.ts
+++ src/routes/api/admin/exercises/get_by_file_name/index.ts
@@ -1,21 +1,22 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 import { Exercises } from "../../../../../models";
-import { AuthRequest } from "../../utils";
+import { Request } from "./interface";
 /**
  * Queries a single file by file name of the owner
  */
-export default async (req: AuthRequest, res: Response) => {
-	const { name } = req.query;
-	try {
-		const { user } = req.body;
-		const doc = await Exercises.findOne({
-			fileName: name,
-			owner: user._id,
-		});
-		res.send(doc.getAdminRepresentation());
-	} catch (error) {
-		res.status(404).send({
-			message: "File not found",
-		});
-	}
+export default async (req: Request, res: Response) => {
+    const { name } = req.query;
+    try {
+        const { user } = req.body;
+
+        const doc = await Exercises.findOne({
+            fileName: name,
+            owner: user._id
+        });
+        res.send(doc.getAdminRepresentation());
+    } catch (error) {
+        res.status(404).send({
+            message: "File not found"
+        });
+    }
 };
