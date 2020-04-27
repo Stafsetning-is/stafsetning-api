5239b65815b918827164ccdedea029103685bc73
njall skarphedinsson <njallis@gmail.com>
2020-04-27 11:52:21 +0000

    changed from req params to query params

--- src/routes/api/admin/exercises/get_by_file_name/index.ts
+++ src/routes/api/admin/exercises/get_by_file_name/index.ts
@@ -1,14 +1,15 @@
 import { Request, Response } from "express";
 import { Exercises } from "../../../../../models";
-
+import { AuthRequest } from "../../utils";
 /**
  * Queries a single file by file name of the owner
  */
-export default async (req: Request, res: Response) => {
+export default async (req: AuthRequest, res: Response) => {
+	const { name } = req.query;
 	try {
 		const { user } = req.body;
 		const doc = await Exercises.findOne({
-			fileName: req.body.fileName,
+			fileName: name,
 			owner: user._id,
 		});
 		res.send(doc.getAdminRepresentation());
