fc36f8fe43174f2bff026534a0734faf490e0ab2
Kjartan Reynir Ólafsson <kjartano17@ru.is>
2020-04-28 20:54:55 +0000

    functionality to change password added

--- src/routes/api/v1/users/index.ts
+++ src/routes/api/v1/users/index.ts
@@ -4,37 +4,43 @@ import getDifficulty from "./get_difficulty";
 import saveExercise from "./save_exercise";
 import unsaveExercise from "./unsave_exercise";
 import savedExercises from "./saved_exercises";
+import changePassword from "./change_password";
 import { RouteBuilder } from "../../../utils";
 
 export default RouteBuilder.routerForEndpoints([
-	{
-		route: "/auth/",
-		controller: auth,
-		method: "get",
-	},
-	{
-		route: "/change_difficulty/",
-		controller: changeDifficulty,
-		method: "post",
-	},
-	{
-		route: "/get_difficulty/",
-		controller: getDifficulty,
-		method: "get",
-	},
-	{
-		route: "/exercises/:id/save",
-		controller: saveExercise,
-		method: "post",
-	},
-	{
-		route: "/exercises/:id/unsave",
-		controller: unsaveExercise,
-		method: "post",
-	},
-	{
-		route: "/exercises/saved",
-		controller: savedExercises,
-		method: "get"
-	}
+    {
+        route: "/auth/",
+        controller: auth,
+        method: "get",
+    },
+    {
+        route: "/change_difficulty/",
+        controller: changeDifficulty,
+        method: "post",
+    },
+    {
+        route: "/get_difficulty/",
+        controller: getDifficulty,
+        method: "get",
+    },
+    {
+        route: "/exercises/:id/save",
+        controller: saveExercise,
+        method: "post",
+    },
+    {
+        route: "/exercises/:id/unsave",
+        controller: unsaveExercise,
+        method: "post",
+    },
+    {
+        route: "/exercises/saved",
+        controller: savedExercises,
+        method: "get",
+    },
+    {
+        route: "/change_password/",
+        controller: changePassword,
+        method: "post",
+    },
 ]);
--- src/routes/api/v1/users/settings/index.ts
+++
@@ -1,9 +0,0 @@
-import { RouteBuilder } from "../../../../utils";
-import changePassword from "./change_password";
-export default RouteBuilder.routerForEndpoints([
-    {
-        route: "/change_password/",
-        controller: changePassword,
-        method: "post",
-    },
-]);
