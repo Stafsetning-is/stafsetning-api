3e763ced8e33b0cdd8d839c678714f847ade339b
njall skarphedinsson <njallis@gmail.com>
2020-05-02 11:45:53 +0000

    fixed comment

--- src/routes/api/admin/users/get_invite_list/index.ts
+++ src/routes/api/admin/users/get_invite_list/index.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from "express";
 import { Users, USER_TYPE_PENDING_ADMIN_INVITE } from "../../../../../models";
 /**
- * sets the user as an admin in the system
+ * gets list of users pending admin priveledges
  */
 export default async (req: Request, res: Response) => {
 	try {
 		const users = await Users.find({ type: USER_TYPE_PENDING_ADMIN_INVITE });
-		res.send(users.map((user) => user.getPublic()));
+		const promises = users.map((user) => user.getPublic());
+		res.send(await Promise.all(promises));
 	} catch (error) {
 		res.status(400).send(error);
 	}
