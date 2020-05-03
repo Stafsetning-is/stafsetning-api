999dc44c9ad519d087ad7e7a71c5544f2126b7bf
njall skarphedinsson <njallis@gmail.com>
2020-05-03 10:42:41 +0000

    fixed font sizes

--- src/models/UserPreferences/utils.ts
+++ src/models/UserPreferences/utils.ts
@@ -15,22 +15,23 @@ export const ALLOWED_BG_COLORS: BackgroundColor[] = [
 ];
 
 export const ALLOWED_FONT_SIZES: FontSize[] = [
-	14,
-	16,
-	18,
-	20,
-	22,
-	24,
 	26,
 	28,
 	30,
 	32,
 	34,
 	36,
+	38,
+	40,
+	42,
+	44,
+	46,
+	48,
+	50,
 ];
 
 export const DEF_BG_COLOR: BackgroundColor = "#F8F8F8";
-export const DEF_FONT_SIZE: FontSize = 18;
+export const DEF_FONT_SIZE: FontSize = 32;
 export const MAX_PREVIEW_TTL = 15;
 export const MIN_PREVIEW_TTL = 2.5;
 export const DEF_PREVIEW_TTL = 5000;
